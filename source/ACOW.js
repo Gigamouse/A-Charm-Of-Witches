8e527359f29da5eb8f7fa06d6097cfc31f1b3008
Rae Palmer <rpalmerarts@gmail.com>
2021-02-23 21:30:44 -0600

    Settings API in place

---
+++ source/ACOW.js
@@ -0,0 +1,55 @@
+// Setting up a list control for the settings property 'theme' w/ callbacks
+var settingThemeNames = ["(none)", "Bright Lights", "Charcoal", "Midnight", "Tinsel City"];
+var settingThemeHandler = function () {
+	// cache the jQuery-wrapped <html> element
+	var $html = $("html");
+
+	// remove any existing theme class
+	$html.removeClass("theme-bright-lights theme-charcoal theme-midnight theme-tinsel-city");
+
+	// switch on the theme name to add the requested theme class
+	switch (settings.theme) {
+	case "Bright Lights":
+		$html.addClass("theme-bright-lights");
+		break;
+	case "Charcoal":
+		$html.addClass("theme-charcoal");
+		break;
+	case "Midnight":
+		$html.addClass("theme-midnight");
+		break;
+	case "Tinsel City":
+		$html.addClass("theme-tinsel-city");
+		break;
+	}
+};
+Setting.addList("theme", {
+	label    : "Choose a theme.",
+	list     : settingThemeNames,
+	onInit   : settingThemeHandler,
+	onChange : settingThemeHandler
+}); // default value not defined, so the first array member "(none)" is used
+
+
+//Changing of Fonts
+var settingFontNames = ["Default", "Alegreya", "Karla"];
+var settingFontHandler = function () {
+    var $html = $("html");
+
+    $html.removeClass("font-alegreya font-karla");
+
+	switch (settings.theme) {
+    case "Alegreya":
+        $html.addClass("font-alegreya");
+        break;
+    case "Karla":
+        $html.addClass("karla");
+        break;
+    }
+};
+Setting.addList("font", {
+label    : "Change the font",
+list     : settingFontNames,
+onInit   : settingFontHandler,
+onChange : settingFontHandler
+});
